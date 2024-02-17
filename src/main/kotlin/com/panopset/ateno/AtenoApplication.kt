8f7dd997daec6a7b4cae376983f427051a8aa330
Karl Dinwiddie <karl.dinwiddie@vsp.com>
2024-02-17 09:14:38 -0800

    spring io base

---
+++ src/main/kotlin/com/panopset/ateno/AtenoApplication.kt
@@ -0,0 +1,11 @@
+package com.panopset.ateno
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AtenoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AtenoApplication>(*args)
+}
